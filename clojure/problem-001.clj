5b3aa343aefeec7fd0e19a4041e18c4d4aefec13
Nelo Onyiah <nelo.onyiah@gmail.com>
2009-06-08 00:32:11 +0100

    Adapted the solution to match that provided ... at least i got it the first time round =)

--- clojure/problem-001.clj
+++ clojure/problem-001.clj
@@ -1,24 +1,14 @@
-;; http://projecteuler.net/index.php?section=problems&id=1
-;; ------------------------------------------------------------------
-;; If we list all the natural numbers below 10 that are multiples
-;; of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-;; Find the sum of all the multiples of 3 or 5 below 1000.
-;; ------------------------------------------------------------------
-
-(defn sum-of-multiples
-  #^{:doc "Retrieve the divisors of n in t"
-     :test (fn [] (assert (= '(1 2 3) (sum-of-multiples 3 10))))}
+(defn sum-divisible-by
+  #^{:doc "Sum of the divisors of n in t"
+     :test (fn [] (assert (= 18 (sum-divisible-by 3 9))))}
   [n t]
-  (* n (apply + (range 1 (+ 1 (unchecked-divide t n))))))
+  (unchecked-divide
+   (* n 
+      (* (unchecked-divide t n)
+	 (+ (unchecked-divide t n) 1))) 2))
 
-(defn problem-001
-  #^{:doc "Solution to problem 1"
-     :test (fn [] (assert (= 233168 (problem-001 3 5 999))))}
-  [x y z]
-  (-
-   (+ (sum-of-multiples x z) (sum-of-multiples y z))
-   ;; should make the subtraction more generic
-   ;; (> (unchecked-divide z (* x y)) 0) should be true
-   (sum-of-multiples (* x y) z)))
-
-(println (problem-001 3 5 999))
+(println
+ (- (+
+     (sum-divisible-by 3 999)
+     (sum-divisible-by 5 999))
+    (sum-divisible-by 15 999)))
