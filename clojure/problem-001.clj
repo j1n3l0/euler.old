1c3602f7301858140a4da7430141a221cc99b50e
Nelo Onyiah <nelo.onyiah@gmail.com>
2009-06-08 09:49:15 +0100

    Minor refactoring

--- clojure/problem-001.clj
+++ clojure/problem-001.clj
@@ -2,13 +2,12 @@
   #^{:doc "Sum of the divisors of n in t"
      :test (fn [] (assert (= 18 (sum-divisible-by 3 9))))}
   [n t]
-  (unchecked-divide
-   (* n 
-      (* (unchecked-divide t n)
-	 (+ (unchecked-divide t n) 1))) 2))
+  (let [p (unchecked-divide t n)]
+    (unchecked-divide
+     (* n (* p (+ p 1))) 2)))
 
+;; Make this more generic
 (println
- (- (+
-     (sum-divisible-by 3 999)
-     (sum-divisible-by 5 999))
+ (- (+ (sum-divisible-by 3 999)
+       (sum-divisible-by 5 999))
     (sum-divisible-by 15 999)))
