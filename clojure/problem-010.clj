65df2944969027613e52a86a1cb0dc8c96f42914
j1n3l0 <nelo.onyiah@gmail.com>
2011-03-17 21:23:35 +0000

    One more attempt at learning clojure, this time with leiningen and testing :)

--- clojure/problem-010.clj
+++
@@ -1,8 +0,0 @@
-;; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-;; Find the sum of all the primes below two million.
-
-;; (defn problem-010
-;;   ""
-;;   [n]
-;;   (loop [max n sum 0]
-;;     (if (< max ))))
